fd67362c9aa2727433eacf85f51e7342c32141d3
FOODOHOLIC <adarshkohli183@gmail.com>
2022-06-26 15:45:33 +0530

    AbstractionDemo

---
+++ src/abstractDemo/Main.java
@@ -0,0 +1,23 @@
+package abstractDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        Son son = new Son(30);
+        son.career();
+
+        Daughter daughter = new Daughter(25);
+        daughter.career();
+
+        Parent mom = new Parent(42) {
+            @Override
+            void career() {
+
+            }
+
+            @Override
+            void partner() {
+
+            }
+        };
+    }
+}
